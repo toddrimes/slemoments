82ef14c506fd7c81ae4a9ab3109155e1ca25af2a
Todd Rimes <todd.rimes@nbcuni.com>
2023-11-01 21:04:01 -0400

    put back env switch on socket URL

--- src/socket.js
+++ src/socket.js
@@ -3,8 +3,8 @@ import {io} from 'socket.io-client/dist/socket.io.js';
 
 const SOCKET_PORT = process.env.SOCKET_PORT || 5501;
 // "undefined" means the URL will be computed from the `window.location` object
-// const URL = process.env.NODE_ENV === 'production' ? 'https://momentsapi-tr-0b46d75889bf.herokuapp.com/' : `http://localhost:${SOCKET_PORT}`;
-const URL = 'https://momentsapi-tr-0b46d75889bf.herokuapp.com/';
+const URL = process.env.NODE_ENV === 'production' ? 'https://momentsapi-tr-0b46d75889bf.herokuapp.com/' : `http://localhost:${SOCKET_PORT}`;
+// const URL = 'https://momentsapi-tr-0b46d75889bf.herokuapp.com/';
 console.log('socket URL is ' + URL);
 export const socket = io(URL, {
     autoConnect: false
